5648a14e1b6e176908b171f4b3271e6afb4ba198
michael garrison <mgarrison.sa@gmail.com>
2020-07-06 16:20:18 -0500

    refactored

--- albums_migration.sql
+++ albums_migration.sql
@@ -6,8 +6,8 @@ CREATE TABLE IF NOT EXISTS albums(
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     artist VARCHAR(100),
     name VARCHAR(100),
-    release_date DATE,
-    sales INT,
-    genre FLOAT,
+    release_date INT,
+    sales FLOAT,
+    genre VARCHAR(100),
     PRIMARY KEY (id)
 );

d2cafd103ce87aef47582f5ad4025c96b5501afe
michael garrison <mgarrison.sa@gmail.com>
2020-07-07 09:16:21 -0500

    basic statements lecture

---
+++ quotes_statement_examples_student.sql
@@ -0,0 +1,41 @@
+USE quotes_db;
+
+
+/*********************** SELECT ***********************/
+
+-- TODO TOGETHER: View the author last name and the quote from quotes. Before the output, add a caption explaining the results.
+
+
+-- TODO TOGETHER: Select all information from the quotes table. Before the output, add a caption explaining the results.
+-- Notice how the id has been auto-incrementing on each insert.
+
+
+/*********************** WHERE ***********************/
+
+-- TODO TOGETHER: View quotes by Douglas Adams.
+
+-- TODO TOGETHER: View quotes by Douglas Adams. Add a caption
+
+-- TODO TOGETHER: View quote with primary key (in this case id) of 5
+
+-- TODO TOGETHER: Select all from quotes where quote contains(is like) "the"
+
+
+
+
+
+/*********************** UPDATE ***********************/
+-- TODO TOGETHER: View all quotes
+
+
+-- TODO TOGETHER: Update quote 4 to first name = Samuel and last name = Clemens
+
+/*********************** DELETE ***********************/
+
+-- TODO TOGETHER: Delete quote with id 3. Then view the output.
+
+
+
+/*********************** TRUNCATE ***********************/
+-- TODO TOGETHER: Truncate quotes table.... there is no going back! Deletes all records.
+

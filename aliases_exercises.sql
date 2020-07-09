febb0a64e2c1e65cd77ee1d16b78bab4c10cdef0
michael garrison <mgarrison.sa@gmail.com>
2020-07-09 10:34:24 -0500

    created file for aliases exercises

---
+++ aliases_exercises.sql
@@ -0,0 +1,13 @@
+USE employees;
+
+# Return 10 employees in a result set named 'full_name' in the format of 'last name, first name' for each employee.
+
+
+
+# Add the date of birth for each employee as 'DOB' to the query
+
+
+
+# Update the query to format full name to include the employee number so it is formatted as 'employee number - last name, first name'
+
+

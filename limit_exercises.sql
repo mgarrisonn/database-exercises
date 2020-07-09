46050be78d5b92a952a881b27dd404404ca2f4b3
michael garrison <mgarrison.sa@gmail.com>
2020-07-08 21:14:55 -0500

    completed exercise

--- limit_exercises.sql
+++ limit_exercises.sql
@@ -2,3 +2,20 @@ USE employees;
 
 SELECT DISTINCT title FROM titles;
 
+SELECT DISTINCT last_name FROM employees
+ORDER BY last_name DESC
+LIMIT 10;
+
+SELECT *
+FROM employees
+WHERE hire_date > '199%'
+    AND birth_date LIKE '%12-25'
+ORDER BY birth_date ASC, hire_date DESC
+LIMIT 5;
+
+SELECT *
+FROM employees
+WHERE hire_date > '199%'
+    AND birth_date LIKE '%12-25'
+ORDER BY birth_date ASC, hire_date DESC
+LIMIT 5 OFFSET 45;

fda2dd52fe009917465b3e2e8281c6fb44765068
michael garrison <mgarrison.sa@gmail.com>
2020-07-09 21:03:01 -0500

    created file for functions exercise

---
+++ functions_exercises.sql
@@ -0,0 +1,10 @@
+USE employees;
+
+# Update your queries for employees whose names start and end with 'E'.
+# Use concat() to combine their first and last name together as a single column in your results.
+
+
+
+# For your query of employees born on Christmas and hired in the 90s,
+# use datediff() to find how many days they have been working at the company
+# (Hint: You will also need to use now() or curdate())

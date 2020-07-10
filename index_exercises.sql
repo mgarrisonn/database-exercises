2b7f6d8ae8d83d45c9088910f3b6abd39b5745cd
michael garrison <mgarrison.sa@gmail.com>
2020-07-09 21:10:16 -0500

    completed index exercises

---
+++ index_exercises.sql
@@ -0,0 +1,14 @@
+USE codeup_test_db;
+
+DROP TABLE IF EXISTS albums;
+
+CREATE TABLE IF NOT EXISTS albums(
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    artist VARCHAR(100),
+    name VARCHAR(100),
+    release_date INT,
+    sales FLOAT,
+    genre VARCHAR(100),
+    PRIMARY KEY (id),
+    UNIQUE KEY (artist, name)
+);
